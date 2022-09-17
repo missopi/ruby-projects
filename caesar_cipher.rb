2aa4c3abfd27ff07d4a0fa1dbe8b7e65b3283c81
missopi <missopi@me.com>
2022-09-17 14:25:16 +0100

    Convert characters to ascii

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -2,6 +2,6 @@ puts "Enter something to be encrypted."
 string = gets.chomp
 
 def caesar_cipher(string, shift_num = 6)
-
+    asciis = string.chars.map { |char| char.ord} # give letters number value
 end
 
