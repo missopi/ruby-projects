15a23e5dc7738d9aa258133d4ca11eb2fae61930
missopi <missopi@me.com>
2022-09-17 14:25:37 +0100

    Shift numbers by shift value

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -3,5 +3,6 @@ string = gets.chomp
 
 def caesar_cipher(string, shift_num = 6)
     asciis = string.chars.map { |char| char.ord} # give letters number value
+    shift_move = asciis.map {|num| num + shift_num} 
 end
 
