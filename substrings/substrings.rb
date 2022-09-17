b59815f671624ec651e788b92bc87ba69cf4e056
missopi <missopi@me.com>
2022-09-17 20:18:10 +0100

    Add dictionary of random words

---
+++ substrings/substrings.rb
@@ -0,0 +1,6 @@
+dictionary = ["hello", "hi", "how", "have", "the", "say", "i", "hand", "are", "is", 
+"are", "you", "me", "we", "yesterday", "his", "your", "what", "find", "what", "when", 
+"is", "other", "where", "find", "can", "go", "yes", "no", "when", "which", "little", 
+"big", "get", "go", "look", "going", "sat", "seven", "cool", "yellow", "sand", "blue", 
+"water", "red", "sky"]
+
