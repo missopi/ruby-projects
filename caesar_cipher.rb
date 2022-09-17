90e05a5029dbb5a53187969269d42f5a8aa2709c
missopi <missopi@me.com>
2022-09-17 14:26:27 +0100

    Turn shifted ascii back into characters

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -4,5 +4,6 @@ string = gets.chomp
 def caesar_cipher(string, shift_num = 6)
     asciis = string.chars.map { |char| char.ord} # give letters number value
     shift_move = asciis.map {|num| num + shift_num} 
+    shifted = shift_move.map {|num| num.chr}.join # turn numbers back to letters
 end
 
