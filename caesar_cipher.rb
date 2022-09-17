2af72b4586632df9f0ca7a01480df40b0c606aa1
missopi <missopi@me.com>
2022-09-17 15:00:36 +0100

    Add conditional for punctuation

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -5,21 +5,24 @@ def caesar_cipher(string, shift_num = 6)
     asciis = string.chars.map { |char| char.ord} # give letters number value
     shift_move = asciis.map {|num| num + shift_num} 
     shift_again = shift_move.map do |num|
-        if num.between?(65, 95) # uppercase letters
+        if num.between?(71, 96) # uppercase letters
           if num > 90
             num - 26
           else 
             num
           end
-        elsif num.between?(97, 127) # lowercase letters
+        elsif num.between?(103, 128) # lowercase letters
           if num > 122
             num - 26
           else
             num
           end
-        elsif num == 38 # keeps spaces the same
-            num - 6
-          end
+        elsif num.between?(38, 70) # keeps spaces and punctuation the same
+            if num == 38 || (num > 38 && num < 54) || (num > 62 && num < 71)
+              num - 6
+            elsif num > 53 && num < 63
+              num + 6
+            end
         end
     end
     shifted = shift_move.map {|num| num.chr}.join # turn numbers back to letters
