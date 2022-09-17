6df72c03a327a689256dbbb9a076df31518710d4
missopi <missopi@me.com>
2022-09-17 14:27:18 +0100

    Return shifted string

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -5,5 +5,8 @@ def caesar_cipher(string, shift_num = 6)
     asciis = string.chars.map { |char| char.ord} # give letters number value
     shift_move = asciis.map {|num| num + shift_num} 
     shifted = shift_move.map {|num| num.chr}.join # turn numbers back to letters
+    puts shifted
 end
+
+caesar_cipher(string)
 
