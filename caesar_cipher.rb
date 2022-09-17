81fdb4a2aade0a971cd2e65c5fa81497a40682f5
missopi <missopi@me.com>
2022-09-17 14:28:40 +0100

    Wrap letters from z back to a

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -4,6 +4,21 @@ string = gets.chomp
 def caesar_cipher(string, shift_num = 6)
     asciis = string.chars.map { |char| char.ord} # give letters number value
     shift_move = asciis.map {|num| num + shift_num} 
+    shift_again = shift_move.map do |num|
+        if num.between?(65, 95) # uppercase letters
+          if num > 90
+            num - 26
+          else 
+            num
+          end
+        elsif num.between?(97, 127) # lowercase letters
+          if num > 122
+            num - 26
+          else
+            num
+          end
+        end
+    end
     shifted = shift_move.map {|num| num.chr}.join # turn numbers back to letters
     puts shifted
 end
