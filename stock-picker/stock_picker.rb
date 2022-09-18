077627702f313f04711b9c20d1ee8b363bb0ecb7
missopi <missopi@me.com>
2022-09-18 21:15:56 +0100

    Loop through stock and p stocks and index

--- stock-picker/stock_picker.rb
+++ stock-picker/stock_picker.rb
@@ -1,7 +1,10 @@
 stocks = []
 
 def stock_picker(stocks)
-
-end
+    stocks.each do |stock|
+      p stock
+    end
+    stocks.each_index { |stock| p stock }
+  end
 
 stock_picker([17,3,6,9,15,8,6,1,10])
