ad587e863114174f90c7e36ade810c290c183cb8
missopi <missopi@me.com>
2022-09-18 13:22:58 +0100

    Delete double words in dictionary

--- substrings/substrings.rb
+++ substrings/substrings.rb
@@ -1,8 +1,7 @@
-dictionary = ["hi", "how", "hen", "the", "say", "i", "hand", "are", "is", 
-"you're", "you", "me", "we", "yesterday", "his", "your", "what", "history", "what", "when", 
-"is", "low", "where", "in", "find", "can", "go", "yes", "no", "when", "which", "handle", 
-"below", "get", "go", "look", "looking", "hat", "day", "and", "sand", "he", 
-"water", "at", "ate", "going"]
+dictionary = ["hi", "how", "hen", "the", "say", "i", "hand", "are", "is", "you're", 
+"you", "me", "we", "yesterday", "his", "your", "history", "what", "when", "low", "where", 
+"in", "find", "can", "go", "yes", "no", "when", "which", "handle", "below", "get", "look", 
+"looking", "hat", "day", "and", "sand", "he", "water", "at", "ate", "going"]
 
 string = ""
 
