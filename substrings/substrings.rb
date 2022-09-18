e8a10a01e8eff7b037a2b3252bee71312b06cc8f
missopi <missopi@me.com>
2022-09-18 20:22:51 +0100

    Tidy up code

--- substrings/substrings.rb
+++ substrings/substrings.rb
@@ -4,15 +4,15 @@ string = ""
 
 def substrings(string, array)
   sub_count = Hash.new { |h, k| h[k] = 0 }
-    array.each do |word|
-      string.downcase.include?(word)
-      if sub_count[word] == nil
-        sub_count[word] = 1
-      else
-        sub_count[word] += 1
-      end
+  array.each do |word|
+    string.downcase.include?(word)
+    if sub_count[word] == nil
+      sub_count[word] = 1
+    else
+      sub_count[word] += 1
     end
-    return sub_count
+  end
+  return sub_count
 end
 
 
