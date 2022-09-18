33dc45807880b20b0b00d8c6bc43cccded8e41e7
missopi <missopi@me.com>
2022-09-18 14:43:03 +0100

    Replace intersection with each, add hash for it
    Intersection compares whole strings, need to compare substrings

--- substrings/substrings.rb
+++ substrings/substrings.rb
@@ -6,9 +6,19 @@ dictionary = ["hi", "how", "hen", "the", "say", "i", "hand", "are", "is", "you'r
 string = ""
 
 def substrings(string, array)
+    sub_count = {}
     string_array = string.downcase.split(" ")
-    sub_compare = string_array.intersection(array)
-    sub_count = sub_compare.tally
+    string_array.each do |string_word|
+        array.each do |word|
+          if string_word.include?(word)
+            if sub_count[word] == nil
+              sub_count[word] = 1
+            else
+              sub_count[word] += 1
+            end
+          end
+        end
+    end
 end
 
 
