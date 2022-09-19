e66a3c35a2a8888945c3b9612e31b405e9ac2168
missopi <missopi@me.com>
2022-09-19 21:58:55 +0100

    Complete if function

--- bubble_sort/bubble_sort.rb
+++ bubble_sort/bubble_sort.rb
@@ -2,9 +2,11 @@ def bubble_sort(array)
     length = array.length
     return array if length <= 1
 
-    
+
     if array[num] > array[num+1]
         array[num],array[num+1] = array[num+1],array[num]
+        sorted = true
+    end
 
 end
 
