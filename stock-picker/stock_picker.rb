5aa3d080f8cd86799a948cf2fb8e9dde1c9eeff3
missopi <missopi@me.com>
2022-09-19 14:59:12 +0100

    Add index to each loop

--- stock-picker/stock_picker.rb
+++ stock-picker/stock_picker.rb
@@ -3,7 +3,7 @@ stocks = []
 def stock_picker(stocks)
     stocks.shift if stocks[0] == stocks.max
     stocks.pop if stocks[-1] == stocks.min
-    stocks.each do |stock|
+    stocks.each_with_index do |stock, index|
       p stock
     end
    
