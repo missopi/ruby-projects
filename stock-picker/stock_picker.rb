95bef2d9648328ad9d2c8b0f0330f9f3724fbd4f
missopi <missopi@me.com>
2022-09-19 18:25:38 +0100

    Find minimum stock values

--- stock-picker/stock_picker.rb
+++ stock-picker/stock_picker.rb
@@ -3,6 +3,7 @@ stocks = []
 def stock_picker(stocks)
     stocks.each_with_index do |stock, index|
         stock_max = stocks[index..].max
+        stock_min = stocks[..index].min
     end
    
     # puts .to_a.inspect
