f5b2fae539dedfe93c4596123c165bfa836cf4c0
missopi <missopi@me.com>
2022-09-19 21:58:29 +0100

    If function to swap numbers when sorted

--- bubble_sort/bubble_sort.rb
+++ bubble_sort/bubble_sort.rb
@@ -2,6 +2,9 @@ def bubble_sort(array)
     length = array.length
     return array if length <= 1
 
+    
+    if array[num] > array[num+1]
+        array[num],array[num+1] = array[num+1],array[num]
 
 end
 
