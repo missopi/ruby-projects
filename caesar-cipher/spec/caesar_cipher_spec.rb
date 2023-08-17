d23b2e2e011af845e88a6f44817cfa5f4251d70f
missopi <missopi@me.com>
2023-08-17 14:47:05 +0100

    Write desciptions of what the tests will be for

--- caesar-cipher/spec/caesar_cipher_spec.rb
+++ caesar-cipher/spec/caesar_cipher_spec.rb
@@ -1,1 +1,18 @@
 require './lib/caesar_cipher'
+
+describe '#caesar_cipher' do
+    it 'returns a coded version of a lowercase word' do
+    end
+
+    it 'returns a coded version of a uppercase and lowercase word' do
+    end
+
+    it 'returns a coded word including numbers' do
+    end
+
+    it 'returns a coded word including symbols' do
+    end
+
+    it 'returns a coded word that includes spaces' do
+    end
+end
