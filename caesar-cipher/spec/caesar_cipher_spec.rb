fed905e2e04c3c5706e5b6ff8285c839b4dce9b7
missopi <missopi@me.com>
2023-08-17 14:55:10 +0100

    Complete Tests
    Some test have failed so I need to fix the original function

--- caesar-cipher/spec/caesar_cipher_spec.rb
+++ caesar-cipher/spec/caesar_cipher_spec.rb
@@ -1,18 +1,23 @@
 require './lib/caesar_cipher'
 
 describe '#caesar_cipher' do
-    it 'returns a coded version of a lowercase word' do
-    end
+  it 'returns a coded version of a lowercase word' do
+    expect(caesar_cipher('hello')).to eql('nkrru')
+  end
 
-    it 'returns a coded version of a uppercase and lowercase word' do
-    end
+  it 'returns a coded version of a uppercase and lowercase word' do
+    expect(caesar_cipher('HEllo')).to eql('NKrru')
+  end
 
-    it 'returns a coded word including numbers' do
-    end
+  it 'returns a coded word including numbers' do
+    expect(caesar_cipher('Hello123')).to eql('Nkrru789')
+  end
 
-    it 'returns a coded word including symbols' do
-    end
+  it 'returns a coded word including symbols' do
+    expect(caesar_cipher('Hello()')).to eql('Nkrru()')
+  end
 
-    it 'returns a coded word that includes spaces' do
-    end
+  it 'returns a coded word that includes spaces' do
+    expect(caesar_cipher('hello HELLO')).to eql('nkrru NKRRU')
+  end
 end
